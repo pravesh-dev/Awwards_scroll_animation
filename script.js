30b0e04871e1e6227d7bb6860a961aac26cb5aea
pravesh dav <humantorch1932@gmail.com>
2024-08-13 16:48:15 +0530

    written loadimage function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const canvas = document.querySelector('#my_canvas');
+const context = canvas.getContext('2d');
+
 const frames = {
   currentIndex: 0,
   maxIndex: 1345,
@@ -19,8 +22,34 @@ function preloadImages() {
                 // startAnimation();
              }
         }
+        images.push(img);
     }
 }
 
+function loadImages(index) {
+    if(index >= 0 && index <= frames.maxIndex){
+        const img = images[index];
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        const scaleX = canvas.width / img.width;
+        const scaleY = canvas.height / img.height;
+        const scale = Math.max(scaleX, scaleY);
+
+        const newWidth = img.width * scale;
+        const newHeight = img.height * scale;
+
+        const offsetX = (canvas.width - newWidth) / 2;
+        const offsetY = (canvas.height - newHeight) / 2;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.imageSmoothingEnabled = true
+        context.imageSmoothingQuality = 'high'
+        context.drawImage(img, offsetX, offsetY, newWidth, newHeight);
+
+        frames.currentIndex = index;
+    }
+}
 
 preloadImages();
